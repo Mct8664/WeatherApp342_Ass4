a96951ce170da4734da4cbbbc9e4455d65418def
faisalmuhabi <fmuhabi@gmail.com>
2022-11-13 00:28:34 -0600

    Assignment5

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.weatherapp
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBar
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.ui.CurrentConditions
+import com.example.weatherapp.ui.ForecastScreen
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController, startDestination = "CurrentConditions") {
+                composable("CurrentConditions") {
+                    CurrentConditions {
+                        navController.navigate("Forecast")
+                    }
+                }
 
+                composable("Forecast") {
+                    ForecastScreen()
+                }
+            }
+        }
     }
 }
